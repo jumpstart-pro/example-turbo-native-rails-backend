188dcbd6e7fa7ecd0f4d03d089587626f7540b08
Joe Masilotti <joe@masilotti.com>
2022-06-14 13:23:17 -0700

    Basic Path Configuration for iOS apps

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 
   resources :posts
 
+  namespace :turbo do
+    namespace :ios do
+      resource :path_configuration, only: :show
+    end
+  end
+
   # Defines the root path route ("/")
   root "posts#index"
 end
