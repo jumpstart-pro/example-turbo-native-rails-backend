ee63256991b14578bef0f758ab096d9033f32226
Joe Masilotti <joe@masilotti.com>
2022-07-05 20:18:17 -0400

    Add /posts tab to iOS, too

--- app/controllers/turbo/ios/path_configurations_controller.rb
+++ app/controllers/turbo/ios/path_configurations_controller.rb
@@ -7,8 +7,13 @@ module Turbo
             tabs: [
               {
                 title: "Home",
-                path: "/",
+                path: root_path,
                 ios_system_image_name: "house"
+              },
+              {
+                title: "Posts",
+                path: posts_path,
+                ios_system_image_name: "square.and.pencil"
               }
             ]
           },
